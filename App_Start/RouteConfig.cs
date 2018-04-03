b05505683fc1862b4ff130ae865f0b931476bbcc
DiWillian <37966950+DiWillian@users.noreply.github.com>
2018-04-03 01:38:44 -0300

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ExemploValidacao
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Todas as Noticias",
+                url: "noticias/",
+                defaults: new { controller = "Noticia", action = "TodasAsNoticias" }
+                );
+
+            routes.MapRoute(
+                name: "Notícias de categoria específica",
+                url: "Noticias/{categoria}",
+                defaults: new { controller = "Noticia", action = "MostraCategoria" }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Pessoa", action = "IndexPessoa", id = UrlParameter.Optional }
+            );
+
+            
+        }
+    }
+}
